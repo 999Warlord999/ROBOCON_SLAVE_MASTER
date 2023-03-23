74009934e3b2b1fdc156b72d42a7915bea5d46c3
Huynh Anh Duy <anhduyhuynh2002@gmail.com>
2023-03-23 15:29:47 +0700

    Pull new code to remove .metadat

---
+++ gun_1.1/Core/Inc/PID.h
@@ -0,0 +1,51 @@
+#include _PID_H_
+#define _PID_H_
+#include "math.h"
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef struct OutVal{
+    double curr;
+    double pre;
+    double P;
+    double I;
+    double I_P;// previous intergration
+    double D;
+    double DF;//fitered derivative
+    double DF_P;//previous fitered derivative
+}OutVal;
+typedef struct PID_Param{
+    uint32_t setVal;
+    uint32_t deltaT;
+    double currVal;
+    double e;
+    double e_p;
+    double kp;
+    double ki;
+    double kd;
+    OutVal *u;
+}PID_Param;
+
+
+#define uI_LLim 10
+#define uI_HLim -10
+#define u_LLim 10
+#define u_HLim -10
+
+#define setVal  pid->setVal
+#define currVal pid->currVal
+#define deltaT  pid->deltaT
+#define e       pid->e
+#define e_p     pid->e_p
+#define kp      pid->kp
+#define ki      pid->ki
+#define kd      pid->kd
+#define uCurr   pid->u->curr
+#define uPre    pid->u->pre
+#define uP      pid->u->P
+#define uI      pid->u->I
+#define uI_P    pid->u->I_P
+#define uD      pid->u->D
+#define uDF     pid->u->DF
+#define uDF_P   pid->u->DF_P
+#endif
